870a938d4cacfced618da82c6a8b4eac28787f02
pavelstarshinov@gmail.com <pav$sta2>
2022-02-19 01:26:35 +0300

    b1_binary_search.py

--- Tasks/b1_binary_search.py
+++ Tasks/b1_binary_search.py
@@ -9,5 +9,21 @@ def binary_search(elem: int, arr: Sequence) -> Optional[int]:
     :param arr: array where element is to be found
     :return: Index of element if it's presented in the arr, None otherwise
     """
+    sorted_arr = sorted(arr)
+    low = 0
+    high = len(arr)-1
+
+    while low <= high:
+        mid = (low + high)//2
+        if sorted_arr[mid] == elem:
+            return arr.index(elem)
+        elif sorted_arr[mid] > elem:
+            high = mid - 1
+        else:
+            low = mid + 1
+
     print(elem, arr)
     return None
+
+if __name__ == "__main__":
+    print(binary_search(100,[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 101]))
