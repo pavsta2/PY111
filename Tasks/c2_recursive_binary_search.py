89e32c89251bcced725cbf15bcde0c594e6e7518
pavel <pavelstarshinov@gmail.com>
2022-02-20 13:08:36 +0300

    c2_recursive_binary_search.py

--- Tasks/c2_recursive_binary_search.py
+++ Tasks/c2_recursive_binary_search.py
@@ -9,5 +9,19 @@ def binary_search(elem: int, arr: Sequence) -> Optional[int]:
     :param arr: array where element is to be found
     :return: Index of element if it's presented in the arr, None otherwise
     """
-    print(elem, arr)
-    return None
+    sorted_arr = sorted(arr)
+    low = 0
+    high = len(sorted_arr) - 1
+    mid = (low + high) // 2
+    if sorted_arr[mid] == elem:
+        return mid
+    if sorted_arr[mid] > elem:
+        arr = sorted_arr[:(mid - 1)]
+        return binary_search(elem, arr)
+    if sorted_arr[mid] < elem:
+        arr = sorted_arr[(mid+1):]
+        return binary_search(elem, arr)
+
+
+if __name__ == "__main__":
+    print(binary_search(7, [1, 4, 5, 6, 7, 3, 4, 8, 2]))
