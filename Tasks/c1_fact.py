aeac59553aa94bf6eca3e9a8dcb1f0cbd7d842cd
pavel <pavelstarshinov@gmail.com>
2022-02-20 22:13:37 +0300

    c1_fact.py

--- Tasks/c1_fact.py
+++ Tasks/c1_fact.py
@@ -4,8 +4,11 @@ def factorial_recursive(n: int) -> int:
     :param n: int > 0
     :return: factorial of n
     """
-    print(n)
-    return 0
+    if n < 0:
+        raise ValueError("n должно быть больше 0")
+    if n == 0:
+        return 1
+    return factorial_recursive(n - 1) * n
 
 
 def factorial_iterative(n: int) -> int:
@@ -15,5 +18,17 @@ def factorial_iterative(n: int) -> int:
     :param n: int > 0
     :return: factorial of n
     """
-    print(n)
-    return 0
+    if n < 0:
+        raise ValueError("n должно быть больше 0")
+    if n == 0:
+        return 1
+    result = 1
+    while n > 0:
+        result *= n
+        n = n - 1
+    return result
+
+
+if __name__ == "__main__":
+    print(factorial_recursive(5))
+    print(factorial_iterative(5))
