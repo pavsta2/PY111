722e7f72dbba6096d006a947b11d1f1d67639b3b
pavel <pavelstarshinov@gmail.com>
2022-03-08 01:27:09 +0300

    e0_breadth_first_search.py

--- Tasks/e0_breadth_first_search.py
+++ Tasks/e0_breadth_first_search.py
@@ -1,5 +1,22 @@
 from typing import Hashable, List
+from collections import deque
+
 import networkx as nx
+from matplotlib import pyplot as plt
+
+
+def draw_graph(graph):
+    pos = nx.spring_layout(graph)
+    nx.draw_networkx_nodes(graph, pos)
+    nx.draw_networkx_labels(graph, pos)
+
+    for edge in graph.edges:
+        nx.draw_networkx_edges(
+            graph, pos,
+            edgelist=[(edge[0], edge[1])], connectionstyle="arc3,rad=0.2"
+            )
+
+    plt.show()
 
 
 def bfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
@@ -10,5 +27,22 @@ def bfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
     :param start_node: starting node for search
     :return: list of nodes in the visited order
     """
-    print(g, start_node)
-    return list(g.nodes)
+    draw_graph(g)
+    path_nodes = []
+    visited_nodes = {node: False for node in g.nodes}  # словарь из непосещенных узлов
+    # конец спава начало очереди слева
+    wait_nodes = deque()  # очередь для узлов
+    wait_nodes.append(start_node)  # подожгли первый узел
+
+    while wait_nodes:
+        current_node = wait_nodes.popleft()  # достаем горящий узел, чтобы поджечь соседей, которые еще не горят
+        path_nodes.append(current_node)  # формируем путь обхода
+        visited_nodes[current_node] = True  # делаем узел сгоревшим
+
+        neighbours = g[current_node]
+        for neighbour in neighbours:
+            if not visited_nodes[neighbour]:
+                wait_nodes.append(neighbour)  # поджигаем соседей
+                visited_nodes[neighbour] = True
+
+    return path_nodes
