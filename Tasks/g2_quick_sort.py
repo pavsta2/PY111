f6a18f035dbc547b4c39cbb188d2f3e3e878e550
pavsta2 <pavelstarshinov@gmail.com>
2022-03-11 20:46:33 +0300

    g2_quick_sort.py

--- Tasks/g2_quick_sort.py
+++ Tasks/g2_quick_sort.py
@@ -1,5 +1,5 @@
 from typing import List
-
+from random import choice
 
 def sort(container: List[int]) -> List[int]:
     """
@@ -8,4 +8,14 @@ def sort(container: List[int]) -> List[int]:
     :param container: container of elements to be sorted
     :return: container sorted in ascending order
     """
-    return container
+
+    if not container:
+        return container
+
+    med_element = choice(container)
+
+    med_element_list = [value for value in container if value == med_element]
+    left_list = [value for value in container if value < med_element]
+    right_list = [value for value in container if value > med_element]
+
+    return sort(left_list) + med_element_list + sort(right_list)
