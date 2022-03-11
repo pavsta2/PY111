b080c69862bc6f33170f2d4bebe02a3f9ae3df8b
pavsta2 <pavelstarshinov@gmail.com>
2022-03-11 21:29:03 +0300

    g2_quick_sort.py with inplace

--- Tasks/g2_quick_sort.py
+++ Tasks/g2_quick_sort.py
@@ -1,5 +1,6 @@
 from typing import List
-from random import choice
+from random import choice, randint
+
 
 def sort(container: List[int]) -> List[int]:
     """
@@ -9,13 +10,38 @@ def sort(container: List[int]) -> List[int]:
     :return: container sorted in ascending order
     """
 
-    if not container:
-        return container
+    def _sort(left_border, right_border) -> None:
+        if left_border >= right_border:
+            return
 
-    med_element = choice(container)
+        random_index = randint(left_border, right_border)
+        pivot = container[random_index]
+        #  choice(container[left_border:right_border]) - не inplace
 
-    med_element_list = [value for value in container if value == med_element]
-    left_list = [value for value in container if value < med_element]
-    right_list = [value for value in container if value > med_element]
+        i, j = left_border, right_border
 
-    return sort(left_list) + med_element_list + sort(right_list)
+        while i <= j:
+            while container[i] < pivot:
+                i += 1
+            while container[j] > pivot:
+                j -= 1
+            if i <= j:
+                container[i], container[j] = container[j], container[i]
+                i += 1
+                j -= 1
+        _sort(left_border, j)
+        _sort(i, right_border)
+
+    _sort(0, len(container) - 1)
+    return container
+
+    # if not container:
+    #     return container
+    #
+    # med_element = choice(container)
+    #
+    # med_element_list = [value for value in container if value == med_element]
+    # left_list = [value for value in container if value < med_element]
+    # right_list = [value for value in container if value > med_element]
+    #
+    # return sort(left_list) + med_element_list + sort(right_list)
